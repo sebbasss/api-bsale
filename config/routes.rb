51a4dae9217566e2ba11a311ecd18c4bd25c0a86
Sebastian Jara <s.jaranilsson@gmail.com>
2022-07-06 16:58:59 -0400

    made a mistake with the routes, fixed it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'categories#index'
-  resources :products, only: %i[index show]
-  resources :categories, only: %i[index show]
+  root 'api/v1/categories#index'
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: %i[index show]
+      resources :categories, only: %i[show]
+    end
+  end
 end
