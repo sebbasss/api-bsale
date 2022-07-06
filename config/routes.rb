9dc30fa96d110d536b45deea1038dc841267eb2f
Sebastian Jara <s.jaranilsson@gmail.com>
2022-07-06 16:53:20 -0400

    default page added for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :products, only: %i[index show]
-      resources :categories, only: %i[show]
-    end
-  end
+  root 'categories#index'
+  resources :products, only: %i[index show]
+  resources :categories, only: %i[index show]
 end
